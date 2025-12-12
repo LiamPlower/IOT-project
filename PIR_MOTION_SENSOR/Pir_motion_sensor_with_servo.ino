df588308e3f2b20feffa6f5e7d84c22f47513042
Liam Plower - STUDENT <g00467316@atu.ie>
2025-12-12 15:24:40 +0000

    Pir_motion_sensor

---
+++ PIR_MOTION_SENSOR/Pir_motion_sensor_with_servo.ino
@@ -0,0 +1,40 @@
+#include <ESP32Servo.h> 
+
+const int PIR_SENSOR_OUTPUT_PIN = 19; /* PIR sensor O/P pin */
+int warm_up;
+
+Servo myServo;
+int servoPin = 18;
+
+void setup() {
+pinMode(PIR_SENSOR_OUTPUT_PIN, INPUT);
+Serial.begin(9600); /* Define baud rate for serial communication */
+        
+myServo.attach(servoPin);
+myServo.write(0); /* starting position*/
+          
+Serial.println("Waiting For Power On Warm Up");
+delay(20000); /* Power On Warm Up Delay */
+Serial.println("Ready!");
+}
+
+void loop() {
+int sensor_output; 
+sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN);
+                 
+if( sensor_output == LOW ) {
+if( warm_up == 1 ) {
+Serial.print("Warming Up\n\n");
+warm_up = 0;
+delay(2000);
+}
+Serial.print("No object in sight\n\n");
+myServo.write(0); /* move servo back */
+delay(1000);
+}
+else
+{
+Serial.print("Object detected\n\n");
+warm_up = 1;
+myServo.write(90); /* move servo when triggered */
+delay(1000); } }
